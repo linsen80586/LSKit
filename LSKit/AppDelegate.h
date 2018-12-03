85c741012163801eebd6d56c47ee2618dd619c33
linsen <linsen80586@163.com>
2018-12-03 11:21:31 +0800

    test

---
+++ LSKit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LSKit
+//
+//  Created by senlin on 2018/12/3.
+//  Copyright © 2018 senlin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LSKit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LSKit
+//
+//  Created by senlin on 2018/12/3.
+//  Copyright © 2018 senlin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
